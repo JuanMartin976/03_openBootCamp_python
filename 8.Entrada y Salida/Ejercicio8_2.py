394d68e4a48912ec27566534785f0e64beaf1d43
Juan <105497430+JuanMartin976@users.noreply.github.com>
2022-07-14 19:49:15 -0300

    Entrada y Salida

---
+++ 8.Entrada y Salida/Ejercicio8_2.py
@@ -0,0 +1,23 @@
+from pickle import *
+
+class Vehiculo:
+
+    def __init__(self, color, puertas):
+        self.color = color
+        self.puertas = puertas
+
+    def __str__(self):
+        return self.color + " " + self.puertas
+
+corsa = Vehiculo("Azul", "4")
+print(corsa)
+
+file = open('vehiculo_objeto.bin', 'w+b')
+
+dump(corsa, file)
+
+file.seek(0)
+nuevo_corsa = load(file)
+
+print(nuevo_corsa)
+file.close()
